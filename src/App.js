da25882cd163a4f37457b5dcf53b6c5a5179f7e0
Bruno Dias <85906470+diasbrunopersonal@users.noreply.github.com>
2024-04-10 14:04:46 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx"
+
+export default function App() {
+  return(
+    <>
+      <Header />
+      <Food />
+      <Footer />
+    </>
+  );
+}
